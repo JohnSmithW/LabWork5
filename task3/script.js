1bb2ee0468321822fb09316f5db4eab73cae97eb
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2019-11-01 14:12:31 +0300

    fixed some issues

--- task3/script.js
+++ task3/script.js
@@ -1,6 +1,9 @@
 function flatArray(ary) {
   var subArray = [];
   for (let i = 0; i < ary.length; i++) {
+    ary = ary.filter(function (el) {
+      return (el !== null && el != NaN && el !== undefined);
+    });
     if (Array.isArray(ary[i])) {
       subArray = subArray.concat(flatArray(ary[i]));
     } else {
