818769b70c3a319047ed60bac217dfb7fef32cbc
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2019-11-08 15:57:37 +0300

    fixed some issues

--- task1/script.js
+++ task1/script.js
@@ -1,5 +1,5 @@
 function replaceString(text, searchStr, newStr) {
-  if (searchStr !== '' && typeof text === 'string' && typeof searchStr === 'string' && typeof newStr ==='string') {
+  if (text !== '' && typeof text === 'string' && typeof searchStr === 'string' && typeof newStr ==='string') {
     searchStr = new RegExp(searchStr, 'g');
     text = text.replace(searchStr, newStr);
     return text;
