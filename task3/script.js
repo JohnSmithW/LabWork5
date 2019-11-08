06404717e21bdd5b097fe2f519677eb5c960c406
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2019-11-08 15:29:10 +0300

    fixed some issues

--- task3/script.js
+++ task3/script.js
@@ -1,5 +1,6 @@
 function flatArray(array) {
   var subArray = [];
+  if (Array.isArray(array)){
   for (let i = 0; i < array.length; i++) {
     array = array.filter(function(el) {
       return (!!el && typeof el[i] !== 'string');
@@ -10,5 +11,6 @@ function flatArray(array) {
       subArray.push(array[i]);
     }
   }
+}
   return subArray;
 }
