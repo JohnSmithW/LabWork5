d5a74cc4e44a95f6dd710a9ec36d79ed844a4987
JohnSmith <salapin.danil1@mail.ru>
2019-11-10 18:13:28 +0000

    fixed some issues

--- task4/script.js
+++ task4/script.js
@@ -1,7 +1,9 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  var start1 = timeRange1;
-  var end1 = timeRange1;
-  var start2 = timeRange2;
-  var end2 = timeRange2;
-  return (start1 <= end2 && start2 <= end1) ? true : false;
+  var start1 = timeRange1[0];
+  var end1 = timeRange1[1];
+  var start2 = timeRange2[0];
+  var end2 = timeRange2[1];
+  if (typeof start1 === 'string' && start1 !== '' && typeof end1 === 'string' && end1 !== '' && typeof start2 === 'string' && start2 !== '' && typeof end2 === 'string' && end2 !== '') {
+    return (start1 <= end2 && start2 <= end1) ? true : false;
+  } else { return false; }
 }
