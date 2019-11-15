1dd98e733f30abcafbe29a71c7554a422074c5d9
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2019-11-15 14:30:25 +0300

    Update script.js

--- task4/script.js
+++ task4/script.js
@@ -4,7 +4,16 @@ function isTimeRangesIntersect(timeRange1, timeRange2) {
   var start2 = timeRange2[0];
   var end2 = timeRange2[1];
   if (typeof start1 === 'string' && start1 !== '' && typeof end1 === 'string' && end1 !== '' && typeof start2 === 'string' && start2 !== '' && typeof end2 === 'string' && end2 !== '') {
-      return (start1 <= end2 && start2 <= end1) ? true : false;
-    } else { return false; }
+    if (start1[0] <= '2' && start1[1] <= '9' && start1[2] === ':' && start1[3] <= '5' && start1[4] <= '9') {
+      if (start2[0] <= '2' && start2[1] <= '9' && start2[2] === ':' && start2[3] <= '5' && start2[4] <= '9') {
+        if (end1[0] <= '2' && end1[1] <= '9' && end1[2] === ':' && end1[3] <= '5' && end1[4] <= '9') {
+          if (end2[0] <= '2' && end2[1] <= '9' && end2[2] === ':' && end2[3] <= '5' && end2[4] <= '9') {
+            return (start1 <= end2 && start2 <= end1) ? true : false;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
   }
 }
